405f8ad49bc4a92315f1c393ac7a5571d1931df1
Wpaswan <dablumcapaswan@gmail.com>
2021-12-21 19:49:35 -0800

    [I added a code where gambler start with 100 rupee]

--- GamblingApp/Program.cs
+++ GamblingApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Gambling
+{
+    int money = 100;
+    int betAmount = 1;
+    int count = 0;
+    int wonCount = 0;
+    public void Start()
+    {
+        while(money!=0 && money !=200)
+        {
+            Console.WriteLine("The bet start");
+            Random rm=new Random();
+            int bet=rm.Next(0,1);
+            count++;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Gambling gambling = new Gambling();
+        
+    }
+}
