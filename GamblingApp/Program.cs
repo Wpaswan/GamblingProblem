459af201083a72390c8a350833e10ca05af3d5c6
Wpaswan <dablumcapaswan@gmail.com>
2021-12-21 21:39:56 -0800

    [I modified the code in which gambler bet with 1 either win or loose]

--- GamblingApp/Program.cs
+++ GamblingApp/Program.cs
@@ -11,12 +11,27 @@
             Console.WriteLine("The bet start");
             Random rm=new Random();
             int bet=rm.Next(0,1);
+            if(bet==1)
+            {
+                Console.WriteLine("Won!!!");
+                money=money+1;
+            }
+            else
+            {
+                Console.WriteLine("lost!!");
+                money=money-1;
+            }
             count++;
+            
+
+
+              
         }
     }
     public static void Main(string[] args)
     {
         Gambling gambling = new Gambling();
+        gambling.Start();
         
     }
 }
